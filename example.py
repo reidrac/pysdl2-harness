10e39f325e6ed9b0019b4257659663d2e1e2b920
Juan J. Martinez <jjm@usebox.net>
2015-05-04 21:54:41 +0100

    some tiles, dragon and knight on the menu

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ game = Game(title="pysdl2 HARNESS demo", width=240, height=240, zoom=3)
 
 background = game.load_resource("background.png")
 title = game.load_resource("title.png")
+tiles = game.load_resource("tiles.png")
 font = game.load_bitmap_font("font.png", width=6, height=10)
 
 dance = game.load_resource("harness-dance.ogg")
@@ -24,10 +25,17 @@ hiscore = 0
 
 class MenuScene(object):
 
+    dragon_frames = ((0, 24, 24, 24), (24, 24, 24, 24))
+    knight_frames = ((48, 48, 24, 24), (72, 48, 24, 24))
+
     def __init__(self):
+        self.intro_channel = None
         self.counter = 2
         self.title_y = -100
-        self.intro_channel = None
+
+        self.anim_delay = 0
+        self.frame = 0
+
         self.boing = game.load_resource("boing.ogg")
         game.play(self.boing)
 
@@ -49,6 +57,10 @@ class MenuScene(object):
             renderer.draw_text(font, 120, 106, "a game by @reidrac",
                     align="center", tint=(98, 100, 220, 255))
 
+            # draw the animation cycle; frame 1 will be drawn 1 pixel higher
+            renderer.draw(tiles, src_rect=self.dragon_frames[self.frame], dest_rect=(12, 192 - self.frame, 24, 24))
+            renderer.draw(tiles, src_rect=self.knight_frames[self.frame], dest_rect=(204, 192 - self.frame, 24, 24))
+
     def update(self, dt):
 
         if self.title_y < 40:
@@ -65,6 +77,11 @@ class MenuScene(object):
         self.counter += dt * 10
         if self.counter > 12:
             self.counter -= 12
+
+        self.anim_delay += dt * 10
+        if self.anim_delay > 1.5:
+            self.anim_delay = 0
+            self.frame = 0 if self.frame else 1
 
         if game.keys[game.KEY_ESCAPE]:
             game.quit()
